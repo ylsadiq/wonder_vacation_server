298611aecd61424d57ff16f9e5cf528e68be7b90
leader with sadiq <gm1sadiq@gmail.com>
2022-06-15 23:54:54 +0600

    payment intent

--- index.js
+++ index.js
@@ -1,5 +1,5 @@
 const express = require('express');
-const {MongoClient} = require('mongodb');
+const { MongoClient, ServerApiVersion } = require('mongodb');
 const ObjectId = require('mongodb').ObjectId
 const cors = require('cors');
 
@@ -10,9 +10,9 @@ const port = process.env.PORT || 5000;
 app.use(cors());
 app.use(express.json())
 
-const uri = "mongodb+srv://wonder-vacation:ELHDc9PpJs5FljdL@cluster0.jbgbo.mongodb.net/myFirstDatabase?retryWrites=true&w=majority";
-const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true });
-
+const uri = "mongodb+srv://wonder-vacation:J8oQ5IhmqMC6bGgs@cluster0.jbgbo.mongodb.net/?retryWrites=true&w=majority";
+const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true, serverApi: ServerApiVersion.v1 });
+const stripe = require("stripe")('sk_test_51Jw4F4HOXxFLrNqIBuXM3R2iCOIn126Q09t07SUGJImydkHBPg6Uxyc9d8bWazIuD9266Ua0EQp7W23ezZVt3Pmi00lIDPjOGy');
 async function run() {
     try {
       await client.connect();
@@ -20,50 +20,77 @@ async function run() {
       const packagesCollection = database.collection("packages");
       const ordersCollection = database.collection("order");
 
-      // GET Packages API
-      app.get('/packages', async(req, res) =>{
-        const cursor = packagesCollection.find({});
-        const packages = await cursor.toArray();
-        res.send(packages);
+  // GET Packages API
+    app.get('/packages', async(req, res) =>{
+    const cursor = packagesCollection.find({});
+    const packages = await cursor.toArray();
+    res.send(packages);
     
       });
-      // GET SINGLE product
+  // GET SINGLE product
     app.get('/packages/:id', async(req, res) =>{
     const id = req.params.id;
-    console.log('getting specific ID', id);
     const query = {_id: ObjectId(id)};
     const package = await packagesCollection.findOne(query);
     res.json(package)
   })
+    
+  // GET Order APi
+  app.get('/order', async(req, res)=>{
+    const email = req.query.email;
+    const query = {email: email};
+    const cursor = ordersCollection.find({'data.email': email })
+    const order = await cursor.toArray()
+    res.json(order)
+  })
+
+  // Packages Post
   app.post('/packages', async(req,res)=>{
     const package = req.body;
     const result = await packagesCollection.insertOne(package)
     res.json(result)
   });
-  app.get('/order', async(req, res) =>{
-    const cursor = ordersCollection.find({});
-    const services = await cursor.toArray();
-    res.send(services); 
-  })
-
+  // Order Post
   app.post("/order", async(req, res) =>{
     const order = req.body
     const result = await ordersCollection.insertOne(order);
     res.send(result)
   })
-    //   create a document to insert
-    //   app.post('/packages',(req, res) =>{
-    //       console.log('hit the post');
-    //       res.send('post Hitted')
-    //   })
-    } finally {
+
+
+  // delete Order
+  app.delete("/order/:id", async (req, res) => {
+    const id = req.params.id;
+    console.log('getting specific ID DELETE', id);
+    const query = { _id: ObjectId(id) };
+    const result = await ordersCollection.deleteOne(query);
+    res.json(result);
+});
+
+app.post("/create-payment-intent", async (req, res) => {
+  const paymentInfo = req.body;
+  const amount = paymentInfo.price * 100;
+  // Create a PaymentIntent with the order amount and currency
+  const paymentIntent = await stripe.paymentIntents.create({
+    currency: "usd",
+    amount: amount,
+    payment_method_types: ['card']
+  });
+console.log(paymentIntent);
+  res.json({
+    clientSecret: paymentIntent.client_secret
+  });
+});
+
+} 
+  finally {
     //   await client.close();
     }
   }
   run().catch(console.dir);
   
   app.get('/', (req,res) =>{
-    res.send('Running my CURD Server')
+    res.send('wonder-vacancy')
 });
 app.listen(port, () =>{
     console.log('Running the server on Port', port);
